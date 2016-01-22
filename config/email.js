528aa871d4646d863d9c97c3d51948b9778fff58
CrookedGrin <github@crookedgrin.com>
2016-01-22 15:58:41 -0600

    Removed database and email config from gitignore, as they're now pulled from .env

---
+++ config/email.js
@@ -0,0 +1,6 @@
+module.exports = {
+	'transportUser' : process.env.EMAIL_USER,
+	'transportPass' : process.env.EMAIL_PASS,
+	'fromAddress'   : 'contactform@crookedgrin.com',
+	'toAddress'     : 'webcontact@crookedgrin.com'
+};
