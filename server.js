b92ada460caaf8c07754fc5abb0638b417f33e29
CrookedGrin <github@crookedgrin.com>
2016-06-13 20:23:32 -0500

    Modified .env loading for local development

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 
+/*
+Load the local .env file so we can run the server locally
+Will not return anything on production, but that's ok because
+the environment vars are already set via Heroku 
+Note that an environment var called NODE_ENV has to be set in .bash_profile
+*/
+if (process.env.NODE_ENV === 'development') {
+	require('dotenv').config();
+}
+
 app.set('view engine', 'jade');
 if (app.get('env') === 'development') {
 	app.locals.pretty = true;
@@ -23,6 +33,7 @@ app.use(session({ secret: 'nobodyknowsthetroubleiseen' }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
+
 
 var configDB = require('./config/database.js');
 var mongoose = require('mongoose');
